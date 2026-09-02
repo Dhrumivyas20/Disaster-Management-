637b61d2bcd1b1e060e2fd456c738aaccd9f6fd4
Dhrumi Vyas <dhrumivyas@Dhrumis-MacBook-Air.local>
2026-09-03 04:23:36 +0530

    Update-3

--- artifacts/api-server/src/routes/index.ts
+++ artifacts/api-server/src/routes/index.ts
@@ -1,10 +1,8 @@
 import { Router, type IRouter } from "express";
-import healthRouter from "./health";
 import pixelalchemyRouter from "./pixelalchemy";
 
 const router: IRouter = Router();
 
-router.use(healthRouter);
 router.use(pixelalchemyRouter);
 
 export default router;
